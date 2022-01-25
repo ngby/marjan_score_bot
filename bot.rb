537b921a100c60847c6efa06fb6459ad4235af5d
matuiy <matuiy33@gmail.com>
2022-01-25 12:56:56 +0900

    bot becomes able to execute a process at the timing that bot joins the server

--- bot.rb
+++ bot.rb
@@ -6,31 +6,34 @@ class Discord
   attr_accessor :bot
 
   def initialize
-    @bot = Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'] ,client_id: ENV['CLIENT_ID'])
-    create_channel
+    @bot = Discordrb::Commands::CommandBot.new(token: ENV['TOKEN'],client_id: ENV['CLIENT_ID'])
+    
   end
 
   def start
-    puts "起動したわよ"
+    puts "start"
     
     setting
 
-    @bot.run
+    bot.run
   end
 
-  def create_channel
-    @bot.event do |event|
-      for cate in event.server.categories
-        exist = true if cate.name == "score"
-      end
-      event.server.create_channel("score", type = 4) unless exist
-      puts "hi"
+  def create_channel(event)
+    for cate in event.server.categories
+      exist = true if cate.name == "score"
     end
+    event.server.create_channel("score", type = 4) unless exist
   end
 
   def setting
+    bot.server_create do |event|
+      create_channel(event)
+    end
     bot.message(with_text: "oha") do |event|
       event.respond "kaisi"
+    end
+    bot.message(with_text: "des") do |event|
+      event.bot.stop
     end
   end
 end
